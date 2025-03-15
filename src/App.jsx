f39b55ab8ef7b3b3c4e6828716e9eea8e4eb8c72
GonzaloRojas <gonzalo.edurojas@gmail.com>
2025-03-15 15:40:43 -0300

    ruta protegida profile

--- src/App.jsx
+++ src/App.jsx
@@ -9,35 +9,58 @@ import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
 import { Route, Routes, useLocation } from "react-router-dom";
 import CartProvider from "./context/CartContext";
-import UserProvider from "./context/UserContext";
+import { UserContext } from "./context/UserContext";
+import { useContext } from "react";
+import ProtectedRoute from "./components/ProtectedRoute";
+import RedirectRoute from "./components/RedirectRoute";
 
 const App = () => {
+  const { user } = useContext(UserContext);
   const location = useLocation();
   const isHome = location.pathname === "/";
   return (
-    <UserProvider>
-      <CartProvider>
-        <div className="app-container">
-          <Navbar />
+    <CartProvider>
+      <div className="app-container">
+        <Navbar />
 
-          {isHome ? (
-            <Home />
-          ) : (
-            <main className="main-content">
-              <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/pizza/:idPizza" element={<Pizza />} />
-                <Route path="/*" element={<NotFound />} />
-                <Route path="/profile" element={<Profile />} />
-              </Routes>
-            </main>
-          )}
-          <Footer />
-        </div>
-      </CartProvider>
-    </UserProvider>
+        {isHome ? (
+          <Home />
+        ) : (
+          <main className="main-content">
+            <Routes>
+              <Route
+                path="/login"
+                element={
+                  <RedirectRoute>
+                    <Login />
+                  </RedirectRoute>
+                }
+              />
+              <Route
+                path="/register"
+                element={
+                  <RedirectRoute>
+                    <Register />
+                  </RedirectRoute>
+                }
+              />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/pizza/:idPizza" element={<Pizza />} />
+              <Route path="/*" element={<NotFound />} />
+              <Route
+                path="/profile"
+                element={
+                  <ProtectedRoute>
+                    <Profile />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </main>
+        )}
+        <Footer />
+      </div>
+    </CartProvider>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,16 @@ import { createRoot } from "react-dom/client";
 import "./main.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import UserProvider from "./context/UserContext.jsx";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </StrictMode>
 );
+
